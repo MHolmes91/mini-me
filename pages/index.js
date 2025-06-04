f19f846827e667a268763c95a7c21de601b0b226
Mark Holmes <contact@markholm.es>
2025-06-03 22:45:19 -0500

    Document Google Fonts setup and use constant (#16)

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import { faAt } from '@fortawesome/free-solid-svg-icons'
 
+const DEFAULT_GOOGLE_FONTS_URL =
+  '//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic|PT+Sans:400,700|PT+Sans+Narrow:400,700|Inconsolata:400'
+
 export async function getStaticProps() {
   const fs = require('fs')
   const path = require('path')
@@ -81,13 +84,26 @@ const icons = {
 }
 
 export default function Home({ data, headerPictureUrl, faviconUrl, faviconType, portfolioEntries, description }) {
+  const styleVars = []
+  const colors = data.style?.colors || {}
+  const fonts = data.style?.fonts || {}
+  if (colors.text) styleVars.push(`--color-text: ${colors.text}`)
+  if (colors.background) styleVars.push(`--color-background: ${colors.background}`)
+  if (colors.accent) styleVars.push(`--color-accent: ${colors.accent}`)
+  if (colors.accentAlt) styleVars.push(`--color-accent-alt: ${colors.accentAlt}`)
+  if (colors.iconBackground) styleVars.push(`--color-icon-background: ${colors.iconBackground}`)
+  if (colors.icon) styleVars.push(`--color-icon: ${colors.icon}`)
+  if (fonts.body) styleVars.push(`--font-body: ${fonts.body}`)
+  if (fonts.header) styleVars.push(`--font-header: ${fonts.header}`)
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{data.pageTitle ? data.pageTitle : data.title}</title>
         {faviconUrl && <link rel="icon" type={faviconType} href={faviconUrl} />}
-        <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic|PT+Sans:400,700|PT+Sans+Narrow:400,700|Inconsolata:400' rel='stylesheet' type='text/css' />
+        <link href={fonts.googleFontsUrl || DEFAULT_GOOGLE_FONTS_URL} rel='stylesheet' type='text/css' />
+        {styleVars.length > 0 && <style>{`:root{${styleVars.join(';')};}`}</style>}
       </Head>
       {data.googleAnalyticsID && (
         <>
